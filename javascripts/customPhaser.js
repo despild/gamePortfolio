a7706a7ed532b3d16c12fd35c5cb2d4b685745c5
despild <despild@gmail.com>
2014-10-21 20:26:52 +0900

    Apply stage fade when press up cursor on door1

--- javascripts/customPhaser.js
+++ javascripts/customPhaser.js
@@ -4,6 +4,11 @@ var player;
 var maps=[];
 var layers=[];
 var stage=0;
+var stageGroups=[];
+var door1,door2,door3,door4;
+var lobbyGroup, stage01Group;
+var isFadeEnded=true;
+var fromTween, toTween;
 function preload(){
 
 
@@ -12,7 +17,8 @@ function preload(){
     pg.load.tilemap('map01', './assets/tilemaps/maps/stage01.json',null,Phaser.Tilemap.TILED_JSON);
     pg.load.image('tiles','./assets/tilemaps/tiles/tileSF20x20x5.png');
     pg.load.image('starfield','./assets/sprites/starfield.png');
-    console.log("Preload Ended");
+    pg.load.image('door','./assets/sprites/lobbyDoorProto.png');
+
 }
 
 function create(){
@@ -20,33 +26,49 @@ function create(){
     pg.physics.startSystem(Phaser.Physics.ARCADE);
     pg.physics.arcade.gravity.y = 500;
 
-
-    starfield = pg.add.tileSprite(0,0,800,600, 'starfield');
     pg.stage.backgroundColor = '#000';
+    starfield = pg.add.tileSprite(0,0,800,600, 'starfield');
+    door1 = pg.add.sprite(20,40,'door');
+    door2 = pg.add.sprite(20,360,'door');
+    door3 = pg.add.sprite(520,40,'door');
+    door4 = pg.add.sprite(520,360,'door');
 
     maps.push(pg.add.tilemap('map00'));
     maps.push(pg.add.tilemap('map01'));
-    
-   console.log("Tilemap adding Ended"); 
 
     for(var i = 0 ;i<maps.length;i++){
         maps[i].addTilesetImage('indexWorld','tiles');
         maps[i].setCollisionBetween(1,5);
     }
-
-   
-    console.log("collisionSetting Ended");
-
-    
+  
     layers.push(maps[0].createLayer('lobby'));
     layers.push(maps[1].createLayer('stage01'));
-
-    console.log("Creating Layer Ended");
 
     for(var i = 0 ; i<maps.length ; i++){
 
         layers[i].resizeWorld();
     }
+
+
+    lobbyGroup = pg.add.group();
+    lobbyGroup.add(layers[0]);
+    lobbyGroup.add(door1);
+    lobbyGroup.add(door2);
+    lobbyGroup.add(door3);
+    lobbyGroup.add(door4);
+    stage01Group = pg.add.group();
+    stage01Group.add(layers[1]);
+
+    stageGroups.push(lobbyGroup);
+    stageGroups.push(stage01Group);
+    for(var i = 0 ; i <stageGroups.length; i++){
+        if(i!=0){
+            stageGroups[i].alpha = 0;
+        }else{
+            stageGroups[i].alpha = 1;
+        }
+    }
+
 
     player = pg.add.sprite(400,30,'player',5);
     pg.physics.arcade.enable(player);
@@ -67,15 +89,19 @@ function create(){
     pg.camera.follow(player);
     
     player.animations.play('front');
+ 
 
-    console.log("Setup Ended");
 }
 
 function update(){
-
+    if(fromTween !=null && toTween !=null ){
+        if(!fromTween.isRunning && !toTween.isRunning){
+            isFadeEnded = true;
+        }
+    }
     player.body.velocity.x = 0;
  
-    stageSetting(stage);   
+    pg.physics.arcade.collide(player, layers[stage]); 
     
     if(pg.input.keyboard.isDown(Phaser.Keyboard.ONE)){
         stage =0;
@@ -84,9 +110,23 @@ function update(){
     }
 
     if(pg.input.keyboard.isDown(Phaser.Keyboard.UP)){
-        fadeOut(player);
+        console.log(isFadeEnded);
+        if(stage===0){
+            if(player.body.x>80&&player.body.x<220){
+                if(player.body.y>100 && player.body.y<260){
+
+                        stageFade(0,1);
+                    
+                }
+            }
+        }else if(stage===1){
+
+                stageFade(1,0);
+
+        }
+        
     }else if(pg.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-        fadeIn(player);
+        
     }
 
     if(pg.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
@@ -153,25 +193,24 @@ function render(){
     //pg.debug.body(player);
 }
 
+function stageFade(from, to){
+    if(fromTween ===undefined || toTween ===undefined){
+        fromTween = pg.add.tween(stageGroups[from]).to({alpha:0},1000,Phaser.Easing.Linear.None,true);
+        toTween = pg.add.tween(stageGroups[to]).to({alpha:1},1000,Phaser.Easing.Linear.None,true);
+        stage = to;
+    }else{
+        if(!fromTween.isRunning && !toTween.isRunning){
+            fromTween = pg.add.tween(stageGroups[from]).to({alpha:0},1000,Phaser.Easing.Linear.None,true);
+            toTween = pg.add.tween(stageGroups[to]).to({alpha:1},1000,Phaser.Easing.Linear.None,true);
+            stage = to;
+        }
+    }
+    
+}
 
 function fadeOut(sprite){
     pg.add.tween(sprite).to({alpha:0},1000,Phaser.Easing.Linear.None,true);
 }
 function fadeIn(sprite){
     pg.add.tween(sprite).to({alpha:1},1000,Phaser.Easing.Linear.None,true);
-}
-
-function stageSetting(stage){
-    for(var i = 0 ; i < layers.length ; i++){
-        if(i === stage){
-            layers[i].alpha = 1.0;        
-        }else{
-            layers[i].alpha = 0.0;
-        }
-        
-    }
-    
-    pg.physics.arcade.collide(player, layers[stage]);
-
-
 }
